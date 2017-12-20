03a53ba8f7e62a4b1a4ec04dfba5746a877a9103
Notyou <arno.bluemel@gmail.com>
2017-12-20 19:35:18 +0100

    Completed Task
    hasLength/2 finally works

--- prolog_lists.pl
+++ prolog_lists.pl
@@ -3,7 +3,7 @@ prepend(E, L, [E | L]).
 addElement(E, L, [L | E]).
 
 hasLength(0, []).
-hasLength(N, [H | T]) :- hasLength(dec(N), T).
+hasLength(N, [H | T]) :- hasLength(M, T), M is N-1.
 
 remove(E, [E | L], L).
 remove(E, [H | T], [Hr | Tr]) :- remove(E, T, Tr).
